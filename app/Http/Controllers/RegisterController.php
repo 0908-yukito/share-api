d5efb875143b7ba661656dc3ec7446d895733062
0908-yukito <tourentiancun992@gmail.com>
2020-06-24 16:48:39 +0900

    message

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Carbon\Carbon;
+
+class RegisterController extends Controller
+{
+ public function post(Request $request) 
+{
+$now = Carbon::now();
+$hashed_password = Hash::make($request->password);
+$param = [
+"name" => $request->name,
+"email" => $request->email,
+"password" => $hashed_password,
+"profile" => $request->profile,
+"created_at" => $now,
+"updated_at" => $now,
+];
+DB::insert('insert into users (name,email,password,profile,created_at,updated_at)
+values (:name,:email,:password,:profile,:created_at,:updated_at)', $param);
+return response()->json();
+}
+}
